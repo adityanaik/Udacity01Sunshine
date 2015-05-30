4dbeaee034a24adcf787387076af578429eb6ca2
Aditya Naik <adityanaik@outlook.com>
2015-05-31 01:02:01 +0530

    use IntentService instead of AsyncTask to update weather

--- app/src/main/java/ga/aditya/udacity01sunshine/app/ForecastFragment.java
+++ app/src/main/java/ga/aditya/udacity01sunshine/app/ForecastFragment.java
@@ -15,6 +15,7 @@
  */
 package ga.aditya.udacity01sunshine.app;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -32,6 +33,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import ga.aditya.udacity01sunshine.app.data.WeatherContract;
+import ga.aditya.udacity01sunshine.app.service.SunshineService;
 
 /**
  * Encapsulates fetching the forecast and displaying it as a {@link ListView} layout.
@@ -174,9 +176,10 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
+                Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
     @Override
