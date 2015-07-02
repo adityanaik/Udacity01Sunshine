ddb0f1819cad3e710a888e85d673098f56c9d5a6
Aditya Naik <adityanaik@outlook.com>
2015-07-02 14:08:05 +0530

    use alarm to schedule the update weather task

--- app/src/main/java/ga/aditya/udacity01sunshine/app/service/SunshineService.java
+++ app/src/main/java/ga/aditya/udacity01sunshine/app/service/SunshineService.java
@@ -17,8 +17,10 @@
 package ga.aditya.udacity01sunshine.app.service;
 
 import android.app.IntentService;
+import android.content.BroadcastReceiver;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -330,6 +332,16 @@ public class SunshineService extends IntentService {
         locationCursor.close();
         // Wait, that worked?  Yes!
         return locationId;
+    }
+
+    static public class AlarmReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent sendIntent = new Intent(context, SunshineService.class);
+            sendIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
+                    intent.getStringExtra(SunshineService.LOCATION_QUERY_EXTRA));
+            context.startService(sendIntent);
+        }
     }
 
 }
