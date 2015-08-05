ea94599236de5739896c6d8ae1146df1e241a9b1
Aditya Naik <adityanaik@outlook.com>
2015-08-05 23:31:27 +0530

    material design - implement support.v7 GrideLayout in detail layout - top

--- app/src/main/java/ga/aditya/udacity01sunshine/app/DetailActivity.java
+++ app/src/main/java/ga/aditya/udacity01sunshine/app/DetailActivity.java
@@ -1,11 +1,7 @@
 package ga.aditya.udacity01sunshine.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,10 +11,6 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_detail);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
@@ -34,27 +26,6 @@ public class DetailActivity extends AppCompatActivity {
                     .add(R.id.weather_detail_container, fragment)
                     .commit();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
